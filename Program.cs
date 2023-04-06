599354d52d0ecaf10317c083f161768eafd8cefc
Alexandr Demyanov <alexm92@inbox.ru>
2023-04-06 17:41:10 +0300

    add code program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string[] ConvertArray(string[] startArray)
+{
+    string str = string.Empty;
+    int maxLengthString = 3;
+    for (int i = 0; i < startArray.Length; i++)
+    {
+        if (startArray[i].Length <= maxLengthString)
+        {
+            str += startArray[i] + " ";
+        }
+    }
+    string[] convertArray = str.Trim().Split(" ");
+    return convertArray;
+}
+
+string[] startArray = {"Hello", "2", "world", ":-)", "Лям"};
+string[] convertedArray = ConvertArray(startArray);
+Console.Write($"[{string.Join(", ", convertedArray)}]");
